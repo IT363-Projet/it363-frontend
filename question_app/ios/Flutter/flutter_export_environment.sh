c9c04e2e94a1b4ec15f80f96504dc344ac636501
Jolann <jmadec@enseirb-matmeca.fr>
2024-11-08 11:39:20 +0100

    Init

---
+++ question_app/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\Users\Jolann\Documents\GitHub\it363-frontend\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Jolann\Documents\GitHub\it363-frontend\question_app"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
